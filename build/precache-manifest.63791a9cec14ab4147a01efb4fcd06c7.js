4582c906d30b8f6a389b833cdd620ea3252b2e5e
phaneendra <phaneendraandukuri@gmail.com>
2020-10-17 16:24:37 +0530

    added assembly simulator static build

---
+++ build/precache-manifest.63791a9cec14ab4147a01efb4fcd06c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baee100cdb8f5b23a62e07f46e85015d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "077fe714af0795cafaca",
+    "url": "/static/css/main.cedb05f6.chunk.css"
+  },
+  {
+    "revision": "93344e7147049440a549",
+    "url": "/static/js/2.d7f80281.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7f80281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077fe714af0795cafaca",
+    "url": "/static/js/main.06a7c453.chunk.js"
+  },
+  {
+    "revision": "24802363cdb31d6bf1e4",
+    "url": "/static/js/runtime-main.0f976f62.js"
+  }
+]);
